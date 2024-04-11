7beaabcf9849c911299dc5b98cccae841bb45a04
itsoluwatobby <oluwasamuel050@gmail.com>
2024-04-11 19:12:39 +0100

    did

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,36 +1,10 @@
 #!/usr/bin/env python3
 """
-module filtered_logger.py
+Script for handling Personal Data
 """
-import logging
+
 from typing import List
 import re
-import os
-import mysql.connector
-import csv
-
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        """ init """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """ formats the record """
-        return filter_datum(self.fields, self.REDACTION,
-                            super(RedactingFormatter, self).format(record),
-                            self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -40,46 +14,3 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
     return message
-
-
-def get_logger() -> logging.Logger:
-    """ returns the logger object """
-    lg = logging.getLogger("user_data")
-    lg.setLevel(logging.INFO)
-    lg.propagate = False
-    sh = logging.StreamHandler()
-    sh.setFormatter(RedactingFormatter(PII_FIELDS))
-    lg.addHandler(sh)
-    return lg
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """function to connect to MySQL DB"""
-    return mysql.connector.connect(
-        host=os.getenv("PERSONAL_DATA_DB_HOST", "root"),
-        database=os.getenv("PERSONAL_DATA_DB_NAME"),
-        user=os.getenv("PERSONAL_DATA_DB_USERNAME", "localhost"),
-        password=os.getenv("PERSONAL_DATA_DB_PASSWORD", ""),
-    )
-
-
-def main():
-    """
-    main function
-    """
-    db = get_db()
-    users = db.cursor()
-    users.execute("SELECT CONCAT('name=', name, ';ssn=', ssn, ';ip=', ip, \
-        ';user_agent', user_agent, ';') AS message FROM users;")
-    formatter = RedactingFormatter(fields=PII_FIELDS)
-    logger = get_logger()
-
-    for user in users:
-        logger.log(logging.INFO, user[0])
-
-    users.close()
-    db.close()
-
-
-if __name__ == "__main__":
-    main()
