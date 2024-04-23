852020888bbd6eb848ee5b48d0cf00c3be991868
itsoluwatobby <oluwasamuel050@gmail.com>
2024-04-23 21:08:33 +0100

    chore/user-authentication task started

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Adds a user to the db
+
+        Arguments:
+            email<str>: user email
+            hashed_password<str>: user hashed_password
+
+        Returns the user object
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        session = self._session
+        session.add(new_user)
+        session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        find a user a and returns the first the user found
+
+        Returns the found user
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+user module
+"""
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """Represents the User class"""
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
+
+    def __repr__(self):
+        """Returns a representation of the user class"""
+        return "<User(id={}, email={})>".format(self.id, self.email)
